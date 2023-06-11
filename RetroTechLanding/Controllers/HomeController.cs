45affbc85de04b867f2ca75b54b0de8c0587f0ea
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-06-12 01:19:37 +0600

    Finalize services page and create Terms and members
    - Create Terms&Members and Model for the page
    - Pass them using array

--- RetroTechLanding/Controllers/HomeController.cs
+++ RetroTechLanding/Controllers/HomeController.cs
@@ -7,39 +7,50 @@ using System.Web.Mvc;
 
 namespace RetroTechLanding.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.CompanyName = "Retro Tech";
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.CompanyName = "Retro Tech";
+			return View();
+		}
 
-        public ActionResult Services() {
-            ServicesModel s1 = new ServicesModel()
-            {
-                Title = "Web Developement",
-                Description = "We provide cutting edge web developement service for your business or company",
-                Cost = "400$"
-            };
+		public ActionResult Services()
+		{
+			ServicesModel s1 = new ServicesModel()
+			{
+				Title = "Web Developement",
+				Description = "We provide cutting edge web developement service for your business or company",
+				Cost = "400$"
+			};
 
-            ServicesModel s2 = new ServicesModel()
-            {
-                Title = "Mobile App Developement",
-                Description = "We will make app for IOS, Android in Dart, swift, or java",
-                Cost = "700$"
-            };
+			ServicesModel s2 = new ServicesModel()
+			{
+				Title = "Mobile App Developement",
+				Description = "We will make app for IOS, Android in Dart, swift, or java",
+				Cost = "700$"
+			};
 
-            ServicesModel[] services = new ServicesModel[] {s1, s2};
+			ServicesModel[] services = new ServicesModel[] { s1, s2 };
 
-            return View(services);
-        }
+			return View(services);
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		public ActionResult Terms_Mems()
+		{
+			MembersModel[] members = new MembersModel[]
+			{
+		new MembersModel("Abir", "22", "C++, ASP.NET, Python"),
+		new MembersModel("Nibir", "18", "Dart, Xamarin"),
+		new MembersModel("Alex", "20", "C#, Java, Python"),
+		new MembersModel("John", "19", "C++, C#, Java"),
+		new MembersModel("Smith", "21", "C++, C#, Java"),
+		new MembersModel("Doe", "22", "C++, C#, Java")
 
-            return View();
-        }
-    }
+			};
+			return View(members);
+		}
+
+		public ActionResult Contact() => View();
+	}
 }
---
+++ RetroTechLanding/Models/MembersModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetroTechLanding.Models
+{
+    public class MembersModel    
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Developer_Lang { get; set; }
+
+        public MembersModel(string name, string age, string devLang)
+        {
+            Name = name;
+            Age = age;
+            Developer_Lang = devLang;
+        }
+    }
+}
