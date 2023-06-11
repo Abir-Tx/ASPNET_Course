d2e409849f845d9664bc900a31083dd0a23f5f05
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-06-12 00:52:49 +0600

    Add dynamic data passing and showing them in bootstrap card

--- RetroTechLanding/Controllers/HomeController.cs
+++ RetroTechLanding/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RetroTechLanding.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,23 @@ namespace RetroTechLanding.Controllers
         }
 
         public ActionResult Services() {
-            return View();
+            ServicesModel s1 = new ServicesModel()
+            {
+                Title = "Web Developement",
+                Description = "We provide cutting edge web developement service for your business or company",
+                Cost = "400$"
+            };
+
+            ServicesModel s2 = new ServicesModel()
+            {
+                Title = "Mobile App Developement",
+                Description = "We will make app for IOS, Android in Dart, swift, or java",
+                Cost = "700$"
+            };
+
+            ServicesModel[] services = new ServicesModel[] {s1, s2};
+
+            return View(services);
         }
 
         public ActionResult Contact()
--- RetroTechLanding/Models/ServicesModel.cs
+++ RetroTechLanding/Models/ServicesModel.cs
@@ -7,6 +7,8 @@ namespace RetroTechLanding.Models
 {
     public class ServicesModel
     {
-
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Cost { get; set; }
     }
 }
