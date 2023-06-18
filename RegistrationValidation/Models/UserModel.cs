5f29bafad1e226c75598ba5d74c7024b4d1beb74
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-06-18 13:22:15 +0600

    Create the user model class in Models

---
+++ RegistrationValidation/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationValidation.Models
+{
+    public class UserModel
+    {
+        [Required (ErrorMessage = "The Name is required")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage = "The Username is required")]
+        public string Username { get; set; }
+        [Required (ErrorMessage = "The gender is required")]
+        public string Gender { get; set; }
+        [Required (ErrorMessage = "You must select a profession")]
+        public string Profession { get; set; }
+        [Required (ErrorMessage = "You must provide your date of birth")]
+        public DateTime Dob { get; set; }
+        [Required (ErrorMessage = "Student ID is required")]
+        public string StudentId { get; set; }
+        [Required (ErrorMessage = "Email is required")]
+        public string Email { get; set;}
+    }
+}
