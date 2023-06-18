ecdc09734ec728012703d19ae10556c24f3a5f94
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-06-18 13:24:11 +0600

    Add an overloaded method of Index for HTTP GET

--- RegistrationValidation/Controllers/HomeController.cs
+++ RegistrationValidation/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RegistrationValidation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,16 @@ namespace RegistrationValidation.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel user)
+        {
+            return View(user);
         }
     }
 }
