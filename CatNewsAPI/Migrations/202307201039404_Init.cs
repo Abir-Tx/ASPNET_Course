123679b2220aae2eaba9d7765b9f70977a5dcc03
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-07-20 17:08:57 +0600

    Add the Migrations folder

---
+++ CatNewsAPI/Migrations/202307201039404_Init.cs
@@ -0,0 +1,29 @@
+﻿namespace CatNewsAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cats",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        CatId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cats");
+        }
+    }
+}
---
+++ CatNewsAPI/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CatNewsAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CatNewsAPI.CFDatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CatNewsAPI.CFDatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
