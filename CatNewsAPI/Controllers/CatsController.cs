92fdb491a7b7381f4abad04a9b8dfa8cab993772
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-07-20 17:05:27 +0600

    Init the cat controller

---
+++ CatNewsAPI/Controllers/CatsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CatNewsAPI;
+using CatNewsAPI.Models;
+
+namespace CatNewsAPI.Controllers
+{
+    public class CatsController : ApiController
+    {
+
+    }
+}
