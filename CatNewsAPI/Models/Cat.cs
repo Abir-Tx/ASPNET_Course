2966064b39f2c2ca8f9b44f22f48582476f5a47d
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-07-20 16:48:59 +0600

    Create a cat Model

---
+++ CatNewsAPI/Models/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatNewsAPI.Models
+{
+    public class Cat
+    {
+        // Cat API properties for the database table
+        public String Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime Date { get; set; }
+        public int CatId { get; set; }
+
+    }
+}
