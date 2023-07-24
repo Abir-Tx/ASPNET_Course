86e64ce64661489077c9fcfe8f72066b2d5e2c20
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-07-24 12:53:59 +0600

    Add AutoMapper and DTO

--- BLL/Services/CategoryService.cs
+++ BLL/Services/CategoryService.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Models;
 using DAL.Repos;
 
 namespace BLL.Services
 {
     public class CategoryService
     {
-        public static dynamic GetAll()
+        public static List<CategoryDTO> GetAll()
         {
-            return CatRepo.GetAll();
+            var data = CatRepo.GetAll();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Category, CategoryDTO>();
+            });
+            var mapper = new Mapper(config);
+            var convertedData = mapper.Map< List < CategoryDTO >> (data);
+            return convertedData;
+
         }
     }
 }
