e5b8b3963b0f1da2752737d520d2f236a655bd97
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-09-03 15:26:04 +0600

    Delete the TIER Arch project and add more docs
    - Deleted one project as it was getting a lot of errors
    - Added more docs for the rest of the projects
    - Added a new migration for the DB for catnewsapi project

---
+++ CatNewsAPI/Migrations/202309030856505_InitDb.cs
@@ -0,0 +1,44 @@
+﻿namespace CatNewsAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.News",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        CatId = c.Int(nullable: false),
+                        Category_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id)
+                .Index(t => t.Category_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.News", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.News", new[] { "Category_Id" });
+            DropTable("dbo.News");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ CatNewsAPI/Readme.md
@@ -0,0 +1,11 @@
+# Cat New API
+
+This project is the starting of the final term lessons. The project is to build an API using MS SQL Server
+
+## API Documentation
+
+Head over here to see the implemented API [docs](https://documenter.getpostman.com/view/21420955/2s9Y5cugPn)
+
+## Notes
+
+Also it is recommended to follow [this guide](https://github.com/Abir-Tx/EzWatchTicketing/blob/main/docs/devdoc.md#open-project) (Which was not written for this project particularly but is similar. I mean you will get the idea if you are a dev)
